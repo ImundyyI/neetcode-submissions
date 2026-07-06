91925afbc505c07ffeeb8220bdde47b3cbcae632
ImundyyI <hazevgamer@gmail.com>
2026-07-06 17:38:42 +1000

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.java
@@ -0,0 +1,75 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        // K X Y Z
+        // V f f t
+
+        /*
+            O U Z O D Y X A Z V
+            . . .
+        */
+        HashMap<Character, Value> hm = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            Value v = hm.getOrDefault(c, new Value(0, 0));
+            v.freq++;
+            hm.put(c, v);
+        }
+        
+
+        int maxLo = 0;
+        int maxHi = 0;
+        int maxLength = Integer.MAX_VALUE;
+        int lo = 0;
+        int count = 0;
+        for (int hi = 0; hi < s.length(); hi++) {
+            Value v = null;
+            if ((v = hm.get(s.charAt(hi))) != null) {
+                v.count++;
+                if (v.count == v.freq) {
+                    count += v.freq;
+                }
+
+            }
+
+            while (count == t.length() && lo <= hi) { 
+                if ((v = hm.get(s.charAt(lo))) != null) {
+                    v.count--;
+                    if (v.count < v.freq) {
+                        count -= v.freq;
+                    }
+                }
+
+                if (hi - lo + 1 < maxLength) {
+                    maxLength = hi - lo + 1;
+                    maxLo = lo;
+                    maxHi = hi;
+                }
+
+                lo++;
+            }
+
+
+        }
+
+        if (maxLength == Integer.MAX_VALUE) {
+            return "";
+        }
+
+        return s.substring(maxLo, maxHi + 1);
+
+        // hm.entrySet().stream().forEach(v -> System.out.println(v.getKey() + ": " + v.getValue().freq));
+        // System.out.println("lo:" + maxLo + " hi: " + maxHi);
+        // return "";
+        
+    }
+}
+
+public class Value {
+    public int freq;
+    public int count;
+
+
+    public Value(int freq, int count) {
+        this.freq = freq;
+        this.count = count;
+    }
+}
